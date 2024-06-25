b3a1844d0c5e8d44cad1df8bac9a17958186529d
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-06-24 21:36:25 -0300

    add api

--- src/main/java/ar/com/code24101/controller/ListarMoviesController.java
+++ src/main/java/ar/com/code24101/controller/ListarMoviesController.java
@@ -1,19 +1,36 @@
 package ar.com.code24101.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ar.com.code24101.domain.Movie;
 import ar.com.code24101.service.MovieService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class ListarMoviesController {
+@WebServlet("/ListarMoviesController")
+public class ListarMoviesController extends HttpServlet{
 
-    public static void main(String[] args) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {        
         //1 crear el service
         MovieService service = new MovieService();
 
         //2 ejecuto el metodo
         ArrayList<Movie> listado = service.listarMovies();
 
-        System.out.println(listado);
+        ObjectMapper mapper = new ObjectMapper();
+
+        //convierto Objecto java a json string
+		//ahora respondo al front: json, Convirtiendo el nuevo Clientes a json
+		String clientesJSON = mapper.writeValueAsString(listado);
+        
+        //mostrar por consola|enviar al frontend
+        resp.getWriter().println(clientesJSON);
+
     }
 }
